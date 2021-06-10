28d75c9d28b51271aa0ad71e2ff3a6f313a90116
KuecuekM <mehmetkucuk74@gmail.com>
2021-06-11 01:11:49 +0200

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,9 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello Feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
+
 
     }
 }
